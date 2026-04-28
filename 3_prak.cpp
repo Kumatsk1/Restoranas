4c8e77c21c8587fcecd150edcca266fe98a828b8
Viacheslav Ivanov <viacheslavivanovklp@gmail.com>
2026-04-28 22:07:39 +0200

    Sukuriau programos logika

--- 3_prak.cpp
+++ 3_prak.cpp
@@ -9,3 +9,70 @@ struct menuItemType {
     double menuPrice;
 };
 
+const int PATIEKALU_KIEKIS = 8;
+
+// nuskaito meniu su tarpais 
+void getData(menuItemType meniuSarasas[]) {
+    ifstream fin("menu.txt");
+    if (!fin) {
+        cout << "Klaida: nepavyko atidaryti menu.txt" << endl;
+    }
+
+    for (int i = 0; i < PATIEKALU_KIEKIS; i++) {
+        string eilute;
+        getline(fin, eilute);
+
+        int pos = eilute.find_last_of(' ');
+        meniuSarasas[i].menuItem = eilute.substr(0, pos);
+        meniuSarasas[i].menuPrice = stod(eilute.substr(pos + 1));
+    }
+
+    fin.close();
+}
+
+// rodo meniu
+void showMenu(menuItemType meniuSarasas[]) {
+    cout << "Sveiki atvykę į restoraną!\n\n";
+    cout << "Pusryčių meniu:\n";
+
+    for (int i = 0; i < PATIEKALU_KIEKIS; i++) {
+        cout << i + 1 << ". " << meniuSarasas[i].menuItem << " - "
+             << fixed << setprecision(2) << meniuSarasas[i].menuPrice << "€" << endl;
+    }
+
+    cout << "\nPasirinkite patiekalus (0 - baigti):\n";
+}
+
+// spausdina sąskaitą 
+void printCheck(menuItemType meniuSarasas[], int kiekiai[]) {
+    ofstream fout("receipt.txt");
+
+    double tarpineSuma = 0.0;
+
+    cout << "\n--- Jūsų užsakymas ---\n\n";
+    fout << "Jūsų užsakymas:\n\n";
+
+    for (int i = 0; i < PATIEKALU_KIEKIS; i++) {
+        if (kiekiai[i] > 0) {
+            double suma = meniuSarasas[i].menuPrice * kiekiai[i];
+            tarpineSuma += suma;
+
+            cout << kiekiai[i] << "  " << meniuSarasas[i].menuItem << "  "
+                 << fixed << setprecision(2) << suma << "€\n";
+
+            fout << kiekiai[i] << "  " << meniuSarasas[i].menuItem << "  "
+                 << fixed << setprecision(2) << suma << "€\n";
+        }
+    }
+
+    double pvm = tarpineSuma * 0.21;
+    double galutine = tarpineSuma + pvm;
+
+    cout << "\nMokesčiai (21%): " << fixed << setprecision(2) << pvm << "€\n";
+    cout << "Galutinė suma: " << fixed << setprecision(2) << galutine << "€\n";
+
+    fout << "\nMokesčiai (21%): " << fixed << setprecision(2) << pvm << "€\n";
+    fout << "Galutinė suma: " << fixed << setprecision(2) << galutine << "€\n";
+
+    fout.close();
+}
